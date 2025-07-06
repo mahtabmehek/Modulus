ed9f13f3e1e88a28be255c4de06b19aa1310cfe1
mahtabmehek <mahtabmehek@gmail.com>
2025-07-06 14:46:06 +0100

    Rename workflow file: deploy-unified.yml -> deploy.yml for simplicity

--- commit-and-deploy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Committing UK region changes...
-git add .
-git commit -m "Update to UK region (eu-west-2) and fix VPC limits"
-git push origin master
-echo Deployment triggered!
-pause
